d7f48fb0e2a83ef4c198fae39ad413c47f9071e9
Tommy <tommyhaskell3@gmail.com>
2023-12-21 19:25:33 -0500

    Define download method & get CO2 emissions dataset

--- import matplotlib.py
+++ import matplotlib.py
@@ -2,3 +2,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
 import numpy as np
+
+# Downloading data set from IBM Cloud
+path= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
+import http.client
+def download(url, filename):
+    connection = http.client.HTTPConnection(url)
+    # using GET method to find the file in cloud
+    connection.request("GET", filename)
+    response = connection.getresponse()
+    with open(filename, "wb") as f:
+        # saving file to local disk
+        f.write(response.read())
+    connection.close()    
+    
+# now calling the function to grab the data
+download(path, "FuelConsumption.csv")
+path = "FuelConsumption.csv"
+    
+# putting the data into a pandas dataframe
+df = pd.read_csv("FuelConsumption.csv")
+
+df.head()
+
+
+            
+    
+
+
+
